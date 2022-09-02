ae441e52bce750cfecfa0c960ac6c2e4ed155d7c
Ha Gia Kinh <kinhhg1@ghtk.co>
2022-09-03 00:49:23 +0700

    add eureka server

--- customer/src/main/java/com/giakinh/customer/CustomerService.java
+++ customer/src/main/java/com/giakinh/customer/CustomerService.java
@@ -21,7 +21,7 @@ public class CustomerService {
                 .email(request.getEmail())
                 .build();
         customerRepository.saveAndFlush(customer);
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}",
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId());
         if(fraudCheckResponse!=null && fraudCheckResponse.isFraudster()){
